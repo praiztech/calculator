f831a5c1402df741c922cb5349166b9b2e84b6bd
Praise Agbabiaka <66212418+praiztech@users.noreply.github.com>
2022-08-10 19:13:38 +0100

    removed extra digits on floats computation

--- scripts/result-funcs.js
+++ scripts/result-funcs.js
@@ -53,7 +53,7 @@ function computebyOperatorPrecedence(dataArr, errormsg1, errormsg2) {
   //start at 1 and end at resultArr.length-2 coz operators are at odd-numbered indexes
   for (let i = 1; i <= resultArr.length-2; i += 2) {
     if (resultArr[i] === '*' || resultArr[i] === '/') {
-      let computed = calculate(resultArr[i-1], resultArr[i], resultArr[i+1]);
+      let computed = compute(resultArr[i-1], resultArr[i], resultArr[i+1]);
       if (computed === 'ZERO DIVISOR') return errormsg2;
       if (computed < minComputableValue || computed > maxComputableValue) return errormsg1;
       resultArr.splice(i-1, 3, computed);
@@ -67,8 +67,8 @@ function computebyOperatorPrecedence(dataArr, errormsg1, errormsg2) {
     let lastResult;
     for (let i = 1; i <= resultArr.length-2; i += 2) {
       let computed = (i === 1 ?
-                      calculate(resultArr[i-1], resultArr[i], resultArr[i+1]) :
-                      calculate(lastResult, resultArr[i], resultArr[i+1]));
+                      compute(resultArr[i-1], resultArr[i], resultArr[i+1]) :
+                      compute(lastResult, resultArr[i], resultArr[i+1]));
       if (computed < minComputableValue || computed > maxComputableValue) return errormsg1;
       lastResult = computed;
     }
@@ -77,7 +77,33 @@ function computebyOperatorPrecedence(dataArr, errormsg1, errormsg2) {
   return result;
 }
 
-function calculate(operand1, operator, operand2) {
+function compute(operand1, operator, operand2) {
+  if ((!operand1.includes('e') && operand1.includes('.')) || (!operand2.includes('e') && operand2.includes('.'))
+  ) return computedFloat(operand1, operator, operand2)
+  return computedIntOrExp(operand1, operator, operand2);
+}
+
+//processes floats as integers to avoid extra decimal places
+function computedFloat(operand1, operator, operand2) {
+  let op1DecimalLength = operand1.includes('.') ? operand1.split('.')[1].length : 0;
+  let op2DecimalLength = operand2.includes('.') ? operand2.split('.')[1].length : 0;
+  let correctionFactor = ((operator === '*') ?
+                          Math.pow(10, op1DecimalLength + op2DecimalLength) :
+                          Math.pow(10, Math.max(op1DecimalLength, op2DecimalLength)));
+  switch (operator) {
+    case '*':
+      return ((operand1 * Math.pow(10, op1DecimalLength)) * (operand2 * Math.pow(10, op2DecimalLength))) / correctionFactor;
+    case '/':
+      if (+operand2 === 0 || +operand2 === -0) return 'ZERO DIVISOR';
+      return (operand1 * correctionFactor) / (operand2 * correctionFactor);
+    case '+':
+      return ((operand1 * correctionFactor) + (operand2 * correctionFactor)) / correctionFactor;
+    case '-':
+      return ((operand1 * correctionFactor) - (operand2 * correctionFactor)) / correctionFactor;
+  }
+}
+
+function computedIntOrExp(operand1, operator, operand2) {
   switch (operator) {
     case '*':
       return +operand1 * +operand2;
