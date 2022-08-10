b8424eccbaaff008fe0daa9603f6963a459dc785
Praise Agbabiaka <66212418+praiztech@users.noreply.github.com>
2022-08-10 03:11:50 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,17 +3,13 @@ import definePointerHighlighting from "./highlight.js";
 import handleKeyboardPress from "./keyboard.js";
 import openDialog from "./dialog.js";
 import processInput from "./process-input.js";
-import {setDefaultTheme, changeTheme} from "./theme.js";
 
 
 customElements.define('preview-data', PreviewData);
-
-setDefaultTheme();
 definePointerHighlighting();
 
 document.addEventListener('keydown', handleKeyboardPress);
 document.addEventListener('keyup', handleKeyboardPress);
 
-document.querySelector('.theme-widget').addEventListener('change', changeTheme);
 document.querySelector('button.open-dialog').addEventListener('click', openDialog);
 document.querySelector('.keypad').addEventListener('click', processInput);
