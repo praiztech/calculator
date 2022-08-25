c0b72ae06cb17e496b5337336e811c5802fee009
praiztech <praisefagbabiaka@gmail.com>
2022-08-25 21:36:30 +0100

    Fixed shortcuts

--- scripts/shtcut.js
+++ scripts/shtcut.js
@@ -16,6 +16,7 @@ export default function handleShtcutKeyPress(evt) {
 
 function handleShtcutKeydown(ctrlKeyPressed, altKeyPressed, key) {
   let keyValue = getKeyValue(ctrlKeyPressed, altKeyPressed, key);
+  console.log(keyValue);
   if (!keyValue) return; //wrong key combination
   
   const btn4key = document.querySelector(`[data-key="${keyValue}"]`);
